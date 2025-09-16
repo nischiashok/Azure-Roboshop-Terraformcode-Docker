8b4d2dcfe645c3c5c0bff19afa42f0611d0c8cf7
Nishchitha A <nischiashok0@gmail.com>
2025-09-16 12:07:51 +0530

    docker code

---
+++ provider.tf
@@ -0,0 +1,12 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "e0be8e24-25e7-4901-ad14-ea389c0f1289"
+}
+
+terraform {
+  backend "azurerm" {}
+}
+provider "vault" {
+  address = "http://vault-int.omshiva.shop:8200"
+  token   = var.token
+}
